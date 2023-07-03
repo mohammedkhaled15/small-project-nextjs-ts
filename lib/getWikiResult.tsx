04be8790a4d1b8d0d21d4b2b3a696a574165b41f
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-07-03 16:41:24 +0300

    ..

---
+++ lib/getWikiResult.tsx
@@ -0,0 +1,19 @@
+export default async function getWikiResult(searchTerm: string) {
+  const searchParams = new URLSearchParams({
+    action: "query",
+    generator: "search",
+    gsrsearch: searchTerm,
+    prop: "pageimagesextracts",
+    exchars: "100",
+    exintro: "true",
+    explaintext: "true",
+    exlimit: "max",
+    format: "json",
+    origin: "*",
+  });
+
+  const response = await fetch(
+    "https://en.wikipedia.org/w/api.php?" + searchParams
+  );
+  return response.json();
+}
